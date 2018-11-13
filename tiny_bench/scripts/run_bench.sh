71aa77ed2293790964db5d2fe2aaad27207dc25c
tianyi93 <harter019@gmail.com>
2018-11-13 15:18:12 -0600

    adding option to configure input size

--- tiny_bench/scripts/run_bench.sh
+++ tiny_bench/scripts/run_bench.sh
@@ -12,7 +12,7 @@ current_time="$(date +"%Y%m%d%H%M")"
 mkdir ../result_${bench_name}_${machine_name}_${current_time}
 cd ../result_${bench_name}_${machine_name}_${current_time}
 
-vector_size=(100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000)
+#vector_size=(100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000)
 thr=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)
 num_run=(1 2 3 4 5 6 7 8 9 10 11)
 
@@ -20,11 +20,11 @@ for run in  "${num_run[@]}"
 do
 	echo "mfc/s,num_threads,vector_size">>openmp_${bench_name}_${run}th_run.csv
 	echo "mfc/s,num_threads,vector_size">>hpxmp_${bench_name}_${run}th_run.csv
-	for size in "${vector_size[@]}"
+	for ((i=5;i<=1000;i+=5));
     	do
 	 	for thread in "${thr[@]}"
 	    	do
-			echo "openmp ${thread} threads, ${size} size, ${run}_th run"
+			echo "openmp ${thread} threads, ${i} size, ${run}_th run"
 			echo "$(LD_PRELOAD=/home/tzhang/pkgs_auto/openmp/build_${compiler}_${machine_name}_release/runtime/src/libomp.so OMP_NUM_THREADS=${thread} ../build/${bench_name} ${size})" "${thread}, ${size}">>openmp_${bench_name}_${run}th_run.csv
 			echo "hpxmp ${thread} threads, ${size} size, ${run}_th run"
 			echo "$(LD_PRELOAD=/home/tzhang/pkgs_auto/hpxMP/build_${compiler}_${machine_name}_release/libhpxmp.so OMP_NUM_THREADS=${thread} ../build/${bench_name} ${size})" "${thread}, ${size}">>hpxmp_${bench_name}_${run}th_run.csv
