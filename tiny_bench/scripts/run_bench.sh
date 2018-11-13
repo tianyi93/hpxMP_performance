d0e1e0557d04faa9cf1d33d2d3d8e814aca27d4a
harter019@gmail.com <harter019@gmail.com>
2018-11-13 10:25:57 -0600

    adding fixed interval data

--- tiny_bench/scripts/run_bench.sh
+++ tiny_bench/scripts/run_bench.sh
@@ -12,7 +12,7 @@ current_time="$(date +"%Y%m%d%H%M")"
 mkdir ../result_${bench_name}_${machine_name}_${current_time}
 cd ../result_${bench_name}_${machine_name}_${current_time}
 
-vector_size=(10000 100000 100000 1000000 10000000 100000000 1000000000 10000000000)
+vector_size=(100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000)
 thr=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)
 num_run=(1 2 3 4 5 6 7 8 9 10 11)
 
@@ -23,9 +23,9 @@ do
 	 	for thread in "${thr[@]}"
 	    	do
 			echo "openmp ${thread} threads, ${size} size, ${run}_th run"
-			echo "$(LD_PRELOAD=/home/tzhang/pkgs_auto/openmp/build_${compiler}_${machine_name}_release/runtime/src/libomp.so OMP_NUM_THREADS=${thread} ../build/${bench_name} ${size})" "${thread}, ${size}">>openmp_${bench_name}_${run}th_run
+			echo "$(LD_PRELOAD=/home/tzhang/pkgs_auto/openmp/build_${compiler}_${machine_name}_release/runtime/src/libomp.so OMP_NUM_THREADS=${thread} ../build/${bench_name} ${size})" "${thread}, ${size}">>openmp_${bench_name}_${run}th_run.csv
 			echo "hpxmp ${thread} threads, ${size} size, ${run}_th run"
-			echo "$(LD_PRELOAD=/home/tzhang/pkgs_auto/hpxMP/build_${compiler}_${machine_name}_release/libhpxmp.so OMP_NUM_THREADS=${thread} ../build/${bench_name} ${size})" "${thread}, ${size}">>hpxmp_${bench_name}_${run}th_run
+			echo "$(LD_PRELOAD=/home/tzhang/pkgs_auto/hpxMP/build_${compiler}_${machine_name}_release/libhpxmp.so OMP_NUM_THREADS=${thread} ../build/${bench_name} ${size})" "${thread}, ${size}">>hpxmp_${bench_name}_${run}th_run.csv
 	    	done
     	done
 done
